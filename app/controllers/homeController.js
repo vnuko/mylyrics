f194da75c1a89eb54b9f0726cefe35d87653f0ad
Milos Vnucko <milosz.milosz@gmail.com>
2018-02-22 22:13:33 +0100

    added lyrics suggestions

--- app/controllers/homeController.js
+++ app/controllers/homeController.js
@@ -1,7 +1,27 @@
 angular.
   module('mylyrics').
-  controller('homeController', function($scope, $location) {
+  controller('homeController', function($scope, $http, $location) {
+    $scope.response = "";
+
+    $scope.onChange = function() {
+      if($scope.search.length < 2) {
+        return false;
+      }
+
+      $http.get('https://api.lyrics.ovh/suggest/'+$scope.search)
+        .then(function(results) { //success
+          $scope.response = results.data.data;
+        }, function(results) { //error
+          $scope.response = results.data.data;
+        });
+    }
+
     $scope.onSearch = function() {
-      $location.path("/" + $scope.artist.toLowerCase() + "/" + $scope.title.toLowerCase());
+      var search_params = $scope.search.split(',');
+      $location.path("/" + search_params[0].trim().toLowerCase() + "/" + search_params[1].trim().toLowerCase());
+    }
+
+    $scope.onLinkClick = function(item) {
+        $location.path("/" + item.artist.name.toLowerCase() + "/" + item.title.toLowerCase());
     }
   });
